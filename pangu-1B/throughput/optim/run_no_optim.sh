7ab3d3203a48875546170376b56846d641c436df
vivianzan <1919922435@qq.com>
2026-01-14 14:12:11 +0800

    throughput-optim

---
+++ pangu-1B/throughput/optim/run_no_optim.sh
@@ -0,0 +1,22 @@
+export ASCEND_RT_VISIBLE_DEVICES=0
+export VLLM_USE_V1=1
+
+HOST=127.0.0.1
+PORT=8000
+
+LOCAL_CKPT_DIR=/opt/pangu/openPangu-Embedded-1B-V1.1
+SERVED_MODEL_NAME=pangu_embedded_1b
+
+vllm serve $LOCAL_CKPT_DIR \
+    --served-model-name $SERVED_MODEL_NAME \
+    --tensor-parallel-size 1 \
+    --trust-remote-code \
+    --host $HOST \
+    --port $PORT \
+    --max-num-seqs 1 \
+    --max-model-len 16384 \
+    --dtype bfloat16 \
+    --distributed-executor-backend mp \
+    --gpu-memory-utilization 0.90 \
+    --no-enable-prefix-caching \
+    --no-enable-chunked-prefill
